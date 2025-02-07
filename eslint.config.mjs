7c2e0749fe269dbf06107f9a23366e97c0ef14ab
Deimos-Na <deeimoss@yandex.ru>
2025-02-07 22:32:14 +0300

    ESlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,13 @@ import pluginJs from "@eslint/js"
 import config from "eslint-config-prettier"
 import plugin from "eslint-plugin-prettier/recommended"
 
-@type {import('eslint').Linter.Config[]}
-export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  config,
-  plugin,
-  {
-    rules: {
-      semi: ["error", "never"],
+/** @type {import('eslint').Linter.Config} */
+export default {
+  languageOptions: { globals: globals.browser },
+  ...pluginJs.configs.recommended,
+  ...config,
+  ...plugin,
+  rules: {
+    semi: ["error", "never"],
   },
-]
+}
